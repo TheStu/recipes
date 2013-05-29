a3af6e074709577ec4b8906262a046f7717595fb
Stuart Morris <stuwashere@gmail.com>
2013-05-29 15:03:41 -0700

    related recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,16 +1,16 @@
 class Recipe < ActiveRecord::Base
-  attr_accessible :calories, :cooking_time, :equipment, :introduction, :picture, :preperation, :servings, 
+  attr_accessible :calories, :cooking_time, :equipment, :introduction, :picture, :preperation, :servings,
   :title, :user, :weight, :pots, :category_ids, :ingredients_attributes, :remove_picture, :average_stars
-  
+
   belongs_to :user
   has_many :categories, :through => :category_joins
   has_many :category_joins, :dependent => :destroy
-  
+
   has_many :ingredients, :dependent => :destroy
   has_many :reviews, :dependent => :destroy
   accepts_nested_attributes_for :category_joins
   accepts_nested_attributes_for :ingredients, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
-  
+
   validates :pots, :inclusion => { :in => ['0', '1', '2', '2+'],
     :message => "needs to be a number between 0 and 2+" }
   validates :cooking_time, :numericality => {:only_integer => true, :message => "is not a number"}
@@ -18,17 +18,17 @@ class Recipe < ActiveRecord::Base
   validates :weight, :numericality => {:only_integer => true, :message => "is not a number"}
   validates :calories, :numericality => {:only_integer => true, :message => "is not a number"}
   validates :introduction, :preperation, :title, :equipment, :presence => true
-  
+
   def to_param
     "#{id}-#{title.parameterize}"
   end
-  
+
   mount_uploader :picture, RecipeUploader
   include PgSearch
   pg_search_scope :search, against: [:title, :introduction, :preperation, :equipment],
     using: {tsearch: {dictionary: "english"}},
     associated_against: {ingredients: :name}
-  
+
   def self.text_search(query)
     if query.present?
       search(query)
@@ -36,7 +36,7 @@ class Recipe < ActiveRecord::Base
       scoped
     end
   end
-  
+
   def self.find_with_categories(*categories)
     return [] if categories.empty?
     join_frags, condition_frags = [], []
@@ -47,6 +47,20 @@ class Recipe < ActiveRecord::Base
     find(:all, :readonly => false,
          :joins => join_frags.join(" "),
          :conditions => condition_frags.join(" AND "))
+  end
+
+  def related
+    self.find_by_categories do |recipes|
+      recipes.sort_by do |p|
+        matched_categories = p.categories.find_all {|t| self.categories.include?(t)}
+        matched_categories.size
+      end.reverse
+    end
+  end
+
+  def find_by_categories
+    category_ids = self.categories.collect{|a| a.id}
+    Recipe.includes(:category_joins).where(["category_joins.category_id IN (?) AND category_joins.recipe_id != ?", categories, self.id])
   end
 end
 
